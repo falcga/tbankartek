86db5b70411a3ce245d9a6020809bdbd6af75c02
falcga <falcga@yandex.ru>
2026-05-18 19:31:59 +0300

    таймаут бд и прокси

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ DB_PATH = 'database.db'
 
 
 def get_db():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH, timeout=10)
     conn.row_factory = sqlite3.Row
     return conn
 
